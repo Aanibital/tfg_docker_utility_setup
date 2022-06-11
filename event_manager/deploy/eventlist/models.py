3c33a285abc611ef866dbedba4cd923f5844ecf1
Aanibital <anibalinf@gmail.com>
2022-06-11 20:31:22 +0200

    Add deploy for event app

---
+++ event_manager/deploy/eventlist/models.py
@@ -0,0 +1,46 @@
+import datetime
+from django.db import models
+from django.contrib.auth.models import User as DjangoUser
+
+class User(models.Model):
+    user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
+    creation_date = models.DateField(auto_now_add=True)
+    last_updated_date = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    def create(cls, username, email, password):
+        user = DjangoUser.objects.create_user(username = username, email = email, password = password)
+        return cls(user = user)
+
+    def __str__(self) -> str:
+        return self.user.username
+
+class EventList(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    creation_date = models.DateField(auto_now_add=True)
+    last_updated_date = models.DateTimeField(auto_now=True)
+    users = models.ManyToManyField(User, blank = True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    event_list = models.ForeignKey(EventList, unique = False, on_delete=models.CASCADE)
+    description = models.TextField()
+    creator = models.ForeignKey(User, unique = False, on_delete=models.PROTECT)
+    date = models.DateTimeField()
+    creation_date = models.DateField(auto_now_add=True)
+    last_updated_date = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField(default=False, blank = True)
+    notes = models.TextField()
+
+    def mark_as_completed(self,username):
+        self.notes = 'List'+ self.event_list.name +'This has been checked by ' + username + ' at ' + str(datetime.datetime.now())
+        self.completed = True
+        self.save()
+    def __str__(self) -> str:
+        return self.name + ' ' + str(self.date)
+
+
+
